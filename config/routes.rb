4e4aa04288f3d06529f5b31af2d8c7127c6693b6
Kazuhiko Shirakawa <kazuhiko.shirakawa@neu-e.biz>
2013-02-05 13:17:43 +0900

    [#43823651] create about page

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 SampleWebsite::Application.routes.draw do
   root :to => 'pages#home'
+  match 'about' => 'pages#about'
 
   # The priority is based upon order of creation:
   # first created -> highest priority.
